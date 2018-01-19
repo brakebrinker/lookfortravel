5285e86aba2dd4c3057b0d7458bb03244fe0eb66
zumtum <drummen11@gmail.com>
2018-01-19 16:53:52 +0300

    second commit

---
+++ wp-content/themes/lookfortravel/header.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta http-equiv="X-RU-Compatible" content="IE=edge">
+	<title><?php wp_title('«', true, 'right'); ?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+	<?php wp_head(); ?>
+</head>
+<body itemscope itemtype="http://schema.org/WebPage">
+	<header>
+	</header>
